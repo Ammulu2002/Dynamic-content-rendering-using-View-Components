c89a14afac1d5fe42e5bb5309b052918ee9e6d91
Gayathri-Ensar <gayathri@ensarsolutions.com>
2025-02-24 20:37:48 +0530

    Data Rendering Changes

--- obj/Debug/net8.0/ViewComponentDemo.AssemblyInfo.cs
+++ obj/Debug/net8.0/ViewComponentDemo.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewComponentDemo")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d8505b6148c466f1564caa8815ff26984c5e3aa9")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewComponentDemo")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewComponentDemo")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
